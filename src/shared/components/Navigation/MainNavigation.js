afea00832e0624de322b2d465d7aab6eb30839b0
nHs2415 <nuwanihansika92@gmail.com>
2025-06-18 12:20:24 +0530

    implementing mobile view

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -3,10 +3,17 @@ import { Link } from 'react-router-dom';
 
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
 import './MainNavigation.css';
 
 const MainNavigation = props => {
   return (
+    <React.Fragment>
+        <SideDrawer>
+        <nav className='main-navigation__drawer-nav'>
+            <NavLinks />
+        </nav>
+    </SideDrawer>
     <MainHeader>
         <button className='main-navigation__menu-btn' onClick={props.onShowSidebar}>
             <span />
@@ -16,11 +23,13 @@ const MainNavigation = props => {
         <h1 className='main-navigation__title'>
             <Link to="/">YourPlaces</Link> 
         </h1>
-        <nav>
+        <nav className='main-navigation__header-nav'> 
             <NavLinks />
         </nav>
     </MainHeader>
-  );
-}
+    </React.Fragment>
+    
+  )
+};
 
 export default MainNavigation;
