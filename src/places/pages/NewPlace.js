3c030b0f0ab2f98d3126d0fd6558fa16ab8ec7d0
nHs2415 <nuwanihansika92@gmail.com>
2025-06-18 08:57:24 +0530

    done project setup with routing and user components

---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const NewPlace = ()  => {
+    return <h1>NewPlace Work!</h1>
+};
+export default NewPlace;
---
+++ src/user/pages/Users.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import UsersList from '../components/UsersList';
+
+const Users = () => {
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Max Schwarz',
+      image: 'https://static.vecteezy.com/system/resources/thumbnails/036/324/708/small/ai-generated-picture-of-a-tiger-walking-in-the-forest-photo.jpg',
+      places: 3
+    }
+  ];
+
+  return (
+    <UsersList items={USERS} />
+  );
+};
+
+export default Users;
