e9278ffa18e6bd6b2c0b6ae2e772e8023a6b5353
nHs2415 <nuwanihansika92@gmail.com>
2025-06-18 10:05:42 +0530

    Styling userItem and other components

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Avatar from "../../shared/components/UIElements/Avatar";
 import './UserItem.css';
 
 const UserItem = props => {
@@ -7,7 +8,7 @@ const UserItem = props => {
         <li className="user-item">
             <div className="user-item__content">
                 <div className="user-item__image">
-                    <img src={props.image} alt={props.name} />
+                    <Avatar image={props.image} alt={props.name} />
                 </div>
                 <div className="user-item__info">
                     <h2>{props.name}</h2>
