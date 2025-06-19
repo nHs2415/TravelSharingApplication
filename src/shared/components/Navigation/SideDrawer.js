a505ddbe7568ee78d2f6d42a601afbabbf2f785d
nHs2415 <nuwanihansika92@gmail.com>
2025-06-19 07:37:12 +0530

    Animating SideDrawer

--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import ReactDOM from 'react-dom';
+import { CSSTransition } from 'react-transition-group';
 
 import './SideDrawer.css';
 
 const SideDrawer = props => {
-  return (
-    <aside className='side-drawer' >
-        {props.children}
-    </aside>
-  )
-}
+  const nodeRef = useRef(null);
 
-export default SideDrawer;   
+  const content = (
+    <CSSTransition 
+      in={props.show} 
+      timeout={200} 
+      classNames='slide-in-left' 
+      mountOnEnter 
+      unmountOnExit
+      nodeRef={nodeRef}
+    >
+      <aside className='side-drawer' onClick={props.onClick }>
+        {props.children}
+      </aside>
+    </CSSTransition>
+  );
+
+  return ReactDOM.createPortal(content, document.getElementById('drawer-hook'));
+};
+
+export default SideDrawer;
