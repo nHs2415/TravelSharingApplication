d3e59a34ebe87daafc9c20854fb2458de45992aa
nHs2415 <nuwanihansika92@gmail.com>
2025-06-20 07:51:13 +0530

    Rendering a map with google map

---
+++ src/shared/components/UIElements/Map.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import './Map.css';
+import L from 'leaflet';
+
+// Fix for missing marker icons in Leaflet
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
+const Map = (props) => {
+  return (
+    <MapContainer
+      center={props.center}
+      zoom={props.zoom}
+      className={`map ${props.className}`}
+      style={props.style}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={props.center}>
+        <Popup>{props.address || 'Location'}</Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
