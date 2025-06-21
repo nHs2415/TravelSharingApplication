df802f82555727331a446fddf4e67a7b56285f7e
nHs2415 <nuwanihansika92@gmail.com>
2025-06-21 07:40:49 +0530

    Adding Authentication and login page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import Auth from './user/pages/Auth';
 import UpdatePlace from './places/pages/UpdatePlace';
 
 const App = () => {
@@ -24,6 +25,9 @@ const App = () => {
           </Route>
           <Route path="/places/:placeId">
             <UpdatePlace />
+          </Route>
+          <Route path="/auth">
+            <Auth />
           </Route>
           <Redirect to="/" />
         </Switch>
