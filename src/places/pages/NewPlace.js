9d0137804754e4fc8a00f47540b580d64b465cfa
nHs2415 <nuwanihansika92@gmail.com>
2025-11-21 14:56:27 +0530

    conecting frontend to backend and sending login request done

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
@@ -6,7 +6,48 @@ import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/Util/valida
 import { useForm } from '../../shared/hooks/form-hook';  
 import './PlaceForm.css';
 
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case 'INPUT_CHANGE' :
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        input: {
+          ...state.inputs,
+          [action.inputId]: {
+            value: action.value, isValid: action.isValid 
+          }
+        },
+        isValid: formIsValid
+      };
+    default:
+      return state;
+  }
+};
+
 const NewPlace = () => {
+
+  useReducer(formReducer, {
+      inputs: {
+        title: {
+          value: '',
+          isValid: false
+        },
+        description: {
+          value: '',
+          isValid: false
+        },
+      },
+      isValid: false
+  });
+
   const [formState, inputHandler] = useForm(
     {
       title: {
@@ -46,7 +87,7 @@ const NewPlace = () => {
         element="textarea"
         label="Description"
         validators={[VALIDATOR_MINLENGTH(5)]}
-        errorText="Please enter at least 5 characters."
+        errorText="Please enter a valid description at least 5 characters."
         onInput={inputHandler}
       />
       <Input
--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -3,6 +3,8 @@ import React, { useState, useContext } from 'react';
 import Card from '../../shared/components/UIElements/Card';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/Util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
 import { AuthContext } from '../../shared/context/Auth-context';
@@ -11,6 +13,8 @@ import './Auth.css';
 const Auth = () => {
     const auth = useContext(AuthContext);
     const [isLoginMode, setIsLoginMode] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
 
     const [formState, inputHandler, setFormData] = useForm({
         email: {
@@ -46,14 +50,76 @@ const Auth = () => {
         setIsLoginMode(prevMode => !prevMode);
     };
 
-    const authSubmitHandler = event => {
+    const authSubmitHandler = async event => {
         event.preventDefault();
-        console.log(formState.inputs); // This will log the form inputs
-        auth.login(); // This is where you would typically handle authentication logic
+        setIsLoading(true);
+
+        if (isLoginMode) {
+            try {
+            setIsLoading(true);
+            const response = await fetch('http://localhost:5000/api/users/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: formState.inputs.email.value,
+                password: formState.inputs.password.value
+            })
+        }); 
+
+            const responseData = await response.json();
+
+            if (!response.ok) {
+                throw new Error(responseData.message);
+            }
+
+            setIsLoading(false);
+            auth.login();
+            } catch (err) {
+            console.log(err);
+            setIsLoading(false);
+            setError(err.message || 'Something went wrong, please try again.');
+            }    
+        } else {
+            try {
+            const response = await fetch('http://localhost:5000/api/users/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: formState.inputs.name.value,
+                email: formState.inputs.email.value,
+                password: formState.inputs.password.value
+            })
+        }); 
+
+            const responseData = await response.json();
+
+            if (!response.ok) {
+                throw new Error(responseData.message);
+            }
+
+            setIsLoading(false);
+            auth.login();
+            } catch (err) {
+            console.log(err);
+            setIsLoading(false);
+            setError(err.message || 'Something went wrong, please try again.');
+            }    
+        }  
+    };
+
+    const errorHandler = () => {
+        setError(null);
     };
 
     return (
+        <React.Fragment>
+        <ErrorModal error={error} onClear={errorHandler} />
         <Card className='authentication'>
+            {isLoading && <LoadingSpinner asOverlay />}
             <h2>Login Required</h2>
             <hr />
             <form onSubmit={authSubmitHandler}>
@@ -92,6 +158,7 @@ const Auth = () => {
             </form>
             <Button inverse onClick={switchModeHandler}>SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}</Button>
         </Card> 
+        </React.Fragment>
     );
 };
 
