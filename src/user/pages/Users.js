e026cd13d5b431a223d41699ac1aed2d33612914
nHs2415 <nuwanihansika92@gmail.com>
2025-11-22 12:13:05 +0530

    creating custom http-hook

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 
 import UsersList from '../components/UsersList';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+
 
 const Users = () => {
-  const USERS = [
-    {
-      id: 'u1',
-      name: 'Max Schwarz',
-      image: 'https://static.vecteezy.com/system/resources/thumbnails/036/324/708/small/ai-generated-picture-of-a-tiger-walking-in-the-forest-photo.jpg',
-      places: 3
-    }
-  ];
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
+
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsLoading(true); 
+      try {
+        const responnse = await fetch('http://localhost:5000/api/users');
+
+        const responseData = await responnse.json();
+
+        if (!responnse.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setLoadedUsers(responseData.users);
+        
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsLoading(false);
+    };  
+    sendRequest();   
+  }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  };
 
   return (
-    <UsersList items={USERS} />
+    <React.Fragment>
+      <ErrorModal error={error} onClear={errorHandler} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
   );
 };
 
